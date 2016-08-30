5797e7ee4ca14b7146f3b03fad38ae37af0758b3
v.milashov <mva730@yandex.ru>
2016-08-30 13:24:34 +0300

    ComposePage XPath

--- TestProject/src/screens/ComposePage.java
+++ TestProject/src/screens/ComposePage.java
@@ -26,13 +26,13 @@ import services.helpers.LetterEnum;
  */
 
 public class ComposePage extends BasePage {
-	@FindBy(css = "textarea.js-input.compose__labels__input")
+	@FindBy(xpath = "//textarea[@class='js-input compose__labels__input']")
 	private WebElement emailField;
 
-	@FindBy(xpath = "//*[contains(@id, 'compose_subj')]")
+	@FindBy(xpath = "//*[@name='Subject']")
 	private WebElement subjectField;
 
-	@FindBy(id = "tinymce")
+	@FindBy(xpath = "//*[@id='tinymce']")
 	private WebElement mailBody;
 
 	@FindBy(xpath = "//*[contains(@id, 'composeEditor_ifr')]")
@@ -41,7 +41,7 @@ public class ComposePage extends BasePage {
 	@FindBy(xpath = "//span[contains(text(), 'Сохранить')]")
 	private WebElement saveButton;
 
-	@FindBy(css = "div[data-name='send']")
+	@FindBy(xpath = "//div[@data-name='send']")
 	private List<WebElement> sendButtons;
 
 	@FindBy(xpath = "//div[@class='b-toolbar__message']")
